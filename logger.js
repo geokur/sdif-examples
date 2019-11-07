0722b44ccc29672a25f28a4bc384129a2ef73911
Georgii Kurtanidze <gku@ciklum.com>
2019-11-07 17:50:54 +0200

    update examples

---
+++ logger.js
@@ -0,0 +1,45 @@
+const { Bindings, Container } = require('sdif')
+
+class InMemoryLogger {
+    constructor() {
+        this.rows = []
+    }
+    log(row) {
+        this.rows.push(row)
+    }
+    printLog() {
+        this.rows.forEach(row => console.log(row))
+    }
+}
+
+class CheckoutService {
+    constructor({ logger, orderService }) {
+        this.logger = logger
+        this.orderService = orderService
+    }
+    checkout(purchase) {
+        this.logger.log(`Checkout purchase: ${purchase}`)
+        this.orderService.createOrder(purchase)
+    }
+}
+
+class OrderService {
+    constructor({ logger }) {
+        this.logger = logger
+    }
+    createOrder(purchase) {
+        this.logger.log(`Create order for: ${purchase}`)
+    }
+}
+
+const bindings = new Bindings()
+bindings.bindClass(CheckoutService, OrderService)
+bindings.bind('logger').toClass(InMemoryLogger).asSingleton()
+
+const container = new Container(bindings)
+const logger = container.getInstance('logger')
+const checkoutService = container.getInstance('checkoutService')
+
+checkoutService.checkout('iPhone 11')
+
+logger.printLog()
